c67345cec5052f130839b48a43074e5fe59c6719
hpazk <hpazk.zkh@gmail.com>
2021-02-05 20:13:42 +0700

    request json payload

--- main.go
+++ main.go
@@ -9,12 +9,38 @@ import (
 
 type M map[string]interface{}
 
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
+
+	e.POST("/users", func(c echo.Context) error {
+		user := new(User)
+		if err := c.Bind(user); err != nil {
+			response := M{
+				"code":    400,
+				"status":  "error",
+				"message": err.Error(),
+			}
+			return c.JSON(http.StatusBadRequest, response)
+		}
+		response := M{
+			"code":    201,
+			"status":  "success",
+			"message": "user succesfully registered",
+			"data":    user,
+		}
+
+		return c.JSON(http.StatusCreated, response)
+	})
 
 	e.GET("/users", func(c echo.Context) error {
 		response := M{
