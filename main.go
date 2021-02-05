d394a0eaf556932ccfa50eb2eb80bdef3bbd266f
hpazk <hpazk.zkh@gmail.com>
2021-02-05 19:39:01 +0700

    logger

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type M map[string]interface{}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.GET("/users", func(c echo.Context) error {
 		response := M{
 			"code":    200,
 			"message": "success",
